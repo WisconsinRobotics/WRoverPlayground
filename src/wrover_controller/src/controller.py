ddcc57b66e25d0c6bccdf35ca66ec9af6a9194a0
Nicholas Underwood <nicholas.underwood@nomorenickels.com>
2022-09-29 18:34:17 -0500

    controller

--- src/wrover_controller/src/controller.py
+++ src/wrover_controller/src/controller.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
-
 import rospy
-from std_msgs.msg import Float64, Int8
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Pose2D
 
-def read_keys(msg):
-    print(msg)
-    throttle = (msg.data >> 3) - (msg.data >> 2 & 1)
-    wheel = (msg.data >> 1 & 1) - (msg.data & 1)
-    wheel_pub.publish(Float64(wheel))
-    throttle_pub.publish(Float64(throttle))
-    left = max(min((throttle + wheel), 1), -1)
-    right = max(min((throttle - wheel), 1), -1)
-    left_pub.publish(Float64(left))
-    right_pub.publish(Float64(right))
 
-rospy.init_node('controller')
-print('load')
-left_pub = rospy.Publisher('rover/cmd/left', Float64, queue_size=10)
-right_pub = rospy.Publisher('rover/cmd/right', Float64, queue_size=10)
-wheel_pub = rospy.Publisher('wheel', Float64, queue_size=10)
-throttle_pub = rospy.Publisher('throttle', Float64, queue_size=10)
+def handle_pose(msg):
+    y = msg.y
 
-print('subs')
-rospy.Subscriber('gui/keys', Int8, read_keys, queue_size=10)
+    if y < 50:
+        left_pub.publish(Float64(1))
+        right_pub.publish(Float64(1))
+    else:
+        left_pub.publish(Float64(0))
+        right_pub.publish(Float64(0))
+
+
+
+rospy.init_node("controller")
+
+left_pub = rospy.Publisher("/rover/cmd/left", Float64, queue_size=10)
+right_pub = rospy.Publisher("/rover/cmd/right", Float64, queue_size=10)
+has_stopped = False
+
+rospy.Subscriber("/rover/pose", Pose2D, handle_pose)
 rospy.spin()
-print('spin')

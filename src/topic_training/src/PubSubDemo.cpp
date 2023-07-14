8cbc4eb2bf33c276b5e0a5d76fa3cb9966679c10
Rus Milovanovic <lazarusmilov@gmail.com>
2023-07-13 20:28:35 -0500

    Working on C++ example code.

---
+++ src/topic_training/src/PubSubDemo.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  //
+} 
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "PubSubDemo");
+
+    ros::NodeHandle n;
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+    ros::spin();
+
+    ros::Rate loop_rate(10);
+
+    int count = 0;
+    while (ros::ok())
+    {
+        std_msgs::String msg;
+
+        std::stringstream ss;
+        ss << "hello world " << count;
+        msg.data = ss.str();
+
+        ROS_INFO("%s", msg.data.c_str());
+
+        chatter_pub.publish(msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        ++count;
+    }
+
+    return 0;
+}

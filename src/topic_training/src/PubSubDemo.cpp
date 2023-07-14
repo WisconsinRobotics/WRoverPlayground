4df5e30e1ab952013d352acc98bf93e20c290ea2
Rus Milovanovic <lazarusmilov@gmail.com>
2023-07-13 21:56:49 -0500

    Fixed build issues with ROS and std_msgs. Got PubSubDemo to run and print hello. The rqt_graph also shows it pointing to itself.

--- src/topic_training/src/PubSubDemo.cpp
+++ src/topic_training/src/PubSubDemo.cpp
@@ -1,43 +1,51 @@
-#include "ros/ros.h"
+#include "ros/init.h"
+#include "ros/node_handle.h"
+#include "ros/rate.h"
+#include "ros/subscriber.h"
+#include "ros/publisher.h"
 #include "std_msgs/String.h"
+
 #include <sstream>
+
+const int QUEUE_SIZE = 1000;
+const int FREQUENCY = 10;
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
-  //
-} 
+}
 
-int main(int argc, char **argv)
+auto main(int argc, char **argv) -> int
 {
-    ros::init(argc, argv, "PubSubDemo");
+  ros::init(argc, argv, "PubSubDemo");
 
-    ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::NodeHandle nhandle;
 
-    ros::spin();
+  ros::Publisher chatter_pub = nhandle.advertise<std_msgs::String>("chatter", QUEUE_SIZE);
+  ros::Subscriber sub = nhandle.subscribe("chatter", QUEUE_SIZE, chatterCallback);
 
-    ros::Rate loop_rate(10);
+  ros::Rate loop_rate(FREQUENCY);
 
-    int count = 0;
-    while (ros::ok())
-    {
-        std_msgs::String msg;
+  int count = 0;
+  while (ros::ok())
+  {
+    std_msgs::String msg;
 
-        std::stringstream ss;
-        ss << "hello world " << count;
-        msg.data = ss.str();
+    std::stringstream sstream;
+    sstream << "hello world " << count;
+    msg.data = sstream.str();
 
-        ROS_INFO("%s", msg.data.c_str());
+    ROS_INFO("%s", msg.data.c_str());
 
-        chatter_pub.publish(msg);
+    chatter_pub.publish(msg);
 
-        ros::spinOnce();
+    ros::spinOnce();
 
-        loop_rate.sleep();
-        ++count;
-    }
+    loop_rate.sleep();
+    ++count;
+  }  
 
-    return 0;
+  ros::spin();
+
+  return 0;
 }

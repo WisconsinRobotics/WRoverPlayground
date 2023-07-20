2f55f3f05170379bfceeb7082ea3677a901ffa04
Rus Milovanovic <lazarusmilov@gmail.com>
2023-07-20 18:31:27 -0500

    Finished the comments for the Python example code and added a few more Optional:'s for the C++ example code.

--- src/topic_training/src/PubSubDemoCPP.cpp
+++ src/topic_training/src/PubSubDemoCPP.cpp
@@ -28,8 +28,8 @@ const int FREQUENCY = 10;
  */
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  /* Print out the message to the terminal. */
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+   /* Print out the message to the terminal. */
+   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 /**
@@ -39,93 +39,98 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
  */
 auto main(int argc, char **argv) -> int
 {
-  /* Initialize ROS and specify the name of the node 
-     (which is the name of the file in this case). 
-     Requires the parameters from the main function 
-     header. */
-  ros::init(argc, argv, "PubSubDemoCPP");
+   /* Initialize ROS and specify the name of the node 
+      (which is the name of the file in this case). 
+      Requires the parameters from the main function 
+      header. */
+   ros::init(argc, argv, "PubSubDemoCPP");
 
-  /* Create a handle for the node. This will do the 
-     actually initialization of the node in the next 
-     few lines. */
-  ros::NodeHandle nhandle;
+   /* Create a handle for the node. This will do the 
+      actually initialization of the node in the next 
+      few lines. */
+   ros::NodeHandle nhandle;
 
-  /* Initialize the Publisher. The advertise() function 
-     is how we tell ROS we want to publish on a topic. 
-     State the message type we will be publishing 
-     (std_msgs::String), the name of the topic 
-     ("chatter"), and the queue size (1000). The queue 
-     size is the maximum number of messages kept as a 
-     buffer before throwing away old ones if we are 
-     publishing too quickly. */
-  ros::Publisher pub = nhandle.advertise<std_msgs::String>("chatter", QUEUE_SIZE);
+   /* Initialize the Publisher. The advertise() function 
+      is how we tell ROS we want to publish on a topic. 
+      State the message type we will be publishing 
+      (std_msgs::String), the name of the topic 
+      ("chatter"), and the queue size (1000). The queue 
+      size is the maximum number of messages kept as a 
+      buffer before throwing away old ones if we are 
+      publishing too quickly. */
+   ros::Publisher pub = nhandle.advertise<std_msgs::String>("chatter", QUEUE_SIZE);
 
-  /* Initialize the Subscriber. The subscriber() function 
-     is how we tell ROS we want to receive messages on 
-     a specific topic. State the name of the topic 
-     ("chatter"), the queue size (1000), and the function 
-     that will be called when a message is received 
-     (chatterCallBack(msg)). */
-  ros::Subscriber sub = nhandle.subscribe("chatter", QUEUE_SIZE, chatterCallback);
+   /* Initialize the Subscriber. The subscriber() function 
+      is how we tell ROS we want to receive messages on 
+      a specific topic. State the name of the topic 
+      ("chatter"), the queue size (1000), and the function 
+      that will be called when a message is received 
+      (chatterCallBack(msg)). */
+   ros::Subscriber sub = nhandle.subscribe("chatter", QUEUE_SIZE, chatterCallback);
 
-  /* Specify the frequency we would like to loop at 
-     (10Hz). */
-  ros::Rate loop_rate(FREQUENCY);
+   /* Specify the frequency we would like to loop at 
+      (10Hz). */
+   ros::Rate loop_rate(FREQUENCY);
 
-  /* A counter of how many messages have been sent. 
-     (Optional: only used for this example to show the 
-     difference in messages) */
-  int count = 0;
-  /* The function ok() will return false if a SIGINT is 
-     received (Ctrl-C), if a shutdown occurs, or if 
-     another issue arises. */
-  while (ros::ok())
-  {
-    /* Declare the message as the type we specified when 
-       we initialized the Publisher (std_msgs::String). */
-    std_msgs::String msg;
+   /* A counter of how many messages have been sent. 
+      (Optional: only used for this example to show the 
+      difference in messages) */
+   int count = 0;
+   /* (Optional: a while loop is not required for
+      Publishers. Only used in this case to show the 
+      continuous timing of publishing messages.) */
+   while (ros::ok())
+   /* The function ok() will return false if a SIGINT is 
+      received (Ctrl-C), if a shutdown occurs, or if 
+      another issue arises. */
+   {
+      /* Declare the message as the type we specified 
+         when we initialized the Publisher 
+         (std_msgs::String). */
+      std_msgs::String msg;
 
-    /* Since we are publishing a message with type 
-       String, we will be following the next few steps 
-       using Stringstream. This will not be the case if 
-       our message type is different. The basic idea is 
-       the same regardless of message type though.*/
+      /* Since we are publishing a message with type 
+         String, we will be following the next few steps 
+         using Stringstream. This will not be the case if 
+         our message type is different. The basic idea is 
+         the same regardless of message type though.*/
     
-    /* Declare the Stringstream. */
-    std::stringstream sstream;
-    /* Initialize the stream with the String (+count) 
-       we want to publish. */
-    sstream << "hello world " << count;
-    /* Set the data of the message to be the stream we 
-       just created. */
-    msg.data = sstream.str();
+      /* Declare the Stringstream. */
+      std::stringstream sstream;
+      /* Initialize the stream with the String (+count) 
+         we want to publish. */
+      sstream << "hello world " << count;
+      /* Set the data of the message to be the stream we 
+         just created. */
+      msg.data = sstream.str();
 
-    /* Print out the message to the terminal. */
-    ROS_INFO("%s", msg.data.c_str());
+      /* Print out the message to the terminal. */
+      ROS_INFO("%s", msg.data.c_str());
 
-    /* Publish the message to the topic for the 
-       Subscriber to receive. */
-    pub.publish(msg);
+      /* Publish the message to the topic for the 
+         Subscriber to receive. */
+      pub.publish(msg);
     
-    /* The function spinOnce() allows us to have a 
-       Publisher and Subscriber in the same file. If it 
-       were not here, our callbacks would never get 
-       called. (Optional: only required if Publisher 
-       and Subscriber are separate) */
-    ros::spinOnce();
+      /* The function spinOnce() allows us to have a 
+         Publisher and Subscriber in the same file. If it 
+         were not here, our callbacks would never get 
+         called. (Optional: only required if Publisher 
+         and Subscriber are separate) */
+      ros::spinOnce();
 
-    /* Use the function sleep() for the time remaining 
-       until we reach our 10Hz publish rate. */
-    loop_rate.sleep();
+      /* Use the function sleep() for the time remaining 
+         until we reach our 10Hz publish rate. (Optional: 
+         only used because we are looping.)*/
+      loop_rate.sleep();
 
-    /* Increment count. (Optional: as stated before) */
-    ++count;
-  }  
+      /* Increment count. (Optional: as stated before) */
+      ++count;
+   }  
 
-  /* The function spin() enters a loop of message 
-     callbacks. It exits once the function ok() returns 
-     false. */
-  ros::spin();
+   /* The function spin() enters a loop of message 
+      callbacks. It exits once the function ok() returns 
+      false. */
+   ros::spin();
 
-  return 0;
+   return 0;
 }
--- src/topic_training/src/PubSubDemoPY.py
+++ src/topic_training/src/PubSubDemoPY.py
@@ -8,51 +8,82 @@ QUEUE_SIZE = 10
 RATE = 10
 
 '''
-This function will get called when a new message has arrived on the topic.
+This function will get called when a new message has 
+arrived on the topic.
 '''
 def callback(data):
     ''' Print out the message to the terminal. '''
     rospy.loginfo('I heard: [%s]', data.data)
 
 ''' 
-The main method that contains the creation of the node, Publisher, Subcriber, and topic. It also describes what we will do with the data/message. 
+The main method that contains the creation of the node, 
+Publisher, Subcriber, and topic. It also describes what 
+we will do with the data/message. 
 '''
 def main():
-    ''' Initialize ROS and specify the name of the node (which is the name of the file in this case). '''
+    ''' Initialize ROS and specify the name of the node 
+        (which is the name of the file in this case). '''
     rospy.init_node('PubSubDemoPY', anonymous=True)
     
-    ''' Initialize the Publisher. State the name of the topic ('chatter'), the message type we will be publishing (String, which is actually in the class std_msgs.msg.String), and the queue size (1000). The queue size is the maximum number of messages kept as a buffer before throwing away old ones if we are publishing too quickly. '''
+    ''' Initialize the Publisher. State the name of the 
+        topic ('chatter'), the message type we will be 
+        publishing (String, which is actually in the 
+        class std_msgs.msg.String), and the queue size 
+        (1000). The queue size is the maximum number of 
+        messages kept as a buffer before throwing away 
+        old ones if we are publishing too quickly. '''
     pub = rospy.Publisher('chatter', String, queue_size=QUEUE_SIZE)
 
-    ''' Initialize the Subscriber. State the name of the topic ('chatter'), the message type we will be publishing (String, which is, again, actually in the class std_msgs.msg.String), and the function that will be called when a message is received (callBack(data)). '''
+    ''' Initialize the Subscriber. State the name of the 
+        topic ('chatter'), the message type we will be 
+        publishing (String, which is, again, actually in 
+        the class std_msgs.msg.String), and the function 
+        that will be called when a message is received 
+        (callBack(data)). '''
     rospy.Subscriber('chatter', String, callback)
 
-    ''' Specify the frequency we would like to loop at (10Hz). '''
+    ''' Specify the frequency we would like to loop at 
+        (10Hz). '''
     rate = rospy.Rate(RATE)
 
-    ''' A counter of how many messages have been sent. (Optional: only used for this example to show the difference in messages) '''
+    ''' A counter of how many messages have been sent. 
+        (Optional: only used for this example to show 
+        the difference in messages) '''
     count = 0
-    '''  '''
+    ''' (Optional: a while loop is not required for
+        Publishers. Only used in this case to show the 
+        continuous timing of publishing messages.) '''
     while not rospy.is_shutdown():
-        '''  '''
+        ''' The function is_shutdown() checks if the program 
+            should exit (Ctrl-C or another issue). '''
+        
+        ''' Create the message of type String. '''
         hello_str = "hello world %s" % count
-        '''  '''
+        ''' Print out the message to the terminal. '''
         rospy.loginfo(hello_str)
 
-        '''  '''
+        ''' Publish the message to the topic for the 
+            Subscriber to receive. '''
         pub.publish(hello_str)
 
-        '''  '''
+        ''' Use the function sleep() for the time 
+            remaining until we reach our 10Hz publish 
+            rate. '''
         rate.sleep()
         
-        '''  '''
+        ''' Increment count. (Optional: as stated 
+            before) '''
         count += 1
 
-    '''  '''
+    ''' The function spin() checks if the node has been 
+        shutdown. If it has, the node will exit. '''
     rospy.spin()
 
 '''
-
+Run main. If a ROSInterruptException is thrown by sleep() 
+when a SIGINT (Ctrl-C) is detected or if the node is 
+shutdown. This keeps us from continuing to execute code 
+after sleep().
 '''
 if __name__ == '__main__':
     try:

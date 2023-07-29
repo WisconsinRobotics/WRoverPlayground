edb8e48bd49f452f8035d61f7c52786d3840ef26
Rus Milovanovic <lazarusmilov@gmail.com>
2023-07-29 17:58:18 -0500

    Added a print statement.

--- src/topic_training/src/MotorChallenge.cpp
+++ src/topic_training/src/MotorChallenge.cpp
@@ -1,3 +1,8 @@
-int main(int argc, char** argv){
-  return 0;
+#include <cstdio>
+#include <iostream>
+
+auto main(int argc, char** argv) -> int
+{
+    std::cout << "Hello, world!\n";
+    return 0;
 }

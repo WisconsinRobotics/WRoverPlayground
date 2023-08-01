2f9628ede9fad3226413a596c6efa382c0c4ecd7
Arthur <arthur.wang200255@outlook.com>
2023-07-31 22:46:54 -0500

    Rename function

--- src/wrover_status_light/src/DemoServer.cpp
+++ src/wrover_status_light/src/DemoServer.cpp
@@ -2,7 +2,7 @@
 #include "ros/init.h"
 #include <wrover_status_light/DoubleService.h>
 
-bool addTwoIntsCallback(wrover_status_light::DoubleServiceRequest& req,
+bool doubleServiceCallback(wrover_status_light::DoubleServiceRequest& req,
                         wrover_status_light::DoubleServiceResponse& res) {
     res.output = req.input * 2;
     return true;
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "double_server");
     ros::NodeHandle nh;
 
-    ros::ServiceServer service = nh.advertiseService("double_service", addTwoIntsCallback);
+    ros::ServiceServer service = nh.advertiseService("double_service", doubleServiceCallback);
 
     ros::spin();
 

7a76c80df4fa838ee0f6eb01485ba1216102826a
Arthur Wang <91100208+Tzanccc@users.noreply.github.com>
2023-09-09 14:52:21 -0500

    Added Service Training Module (#9)
    * Created new package for service tutorial
    
    * Began writeup for ROS service tutorial
    
    * Moved package folder
    
    * New folder
    
    * Finished service intro writeup
    
    * Completed python demo code, started C++ demo code
    
    * Completed C++ sample code implementation
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Rewording to simplify
    
    * Added language types to codeblocks
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Added tables
    
    * More changes
    
    * Bumped up to C++20
    
    * Update src/wrover_status_light/src/DemoClient.cpp
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Added link to ROS tutorials on services
    
    * Rename function
    
    * Added 'what you should know' and 'what you will learn' section to mirror topic training, moved code explanation to source files
    
    * Added comments and instructions on how to run the code in markdown file
    
    * Added exception handling
    
    * Completed challenge writeup and added starter files
    
    * Update src/wrover_status_light/ServiceIntro.md
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    
    * Update src/wrover_status_light/src/DemoClient.py
    
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
    
    * Changed example code order for C++ server
    
    * Added short paragraph on defining constants
    
    * Added new service
    
    ---------
    
    Co-authored-by: srao32 <55770703+srao32@users.noreply.github.com>
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>

---
+++ src/wrover_status_light/src/DemoClient.cpp
@@ -0,0 +1,67 @@
+#include "ros/duration.h"
+#include "ros/ros.h"
+#include "ros/init.h"
+#include "ros/node_handle.h"
+#include "ros/service.h"
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <wrover_status_light/DoubleService.h>
+
+int main(int argc, char** argv) {
+    /*
+    The ros::init() function sets up the node and needs to be called before using any other parts 
+    of the ROS library. Here it takes in 3 arguments: argc, argv, and the name of this node. 
+    */
+    ros::init(argc, argv, "double_client");
+
+    /*
+    A NodeHandle is the main access point to communications with ROS in C++. Here it is used to 
+    create a new server.
+    */
+    ros::NodeHandle nh;
+
+    /*
+    Creates a new client instance using the NodeHandle. Here it specifies the service type 
+    "wrover_status_light::DoubleService" and service name "double_service".
+    */
+    ros::ServiceClient client = nh.serviceClient<wrover_status_light::DoubleService>("double_service");
+
+    /*
+    Initializes various local variables. rate specifies the frequency we would want to run at, and 
+    srv is an object for the service (which envelopes accessing the request and response fields).
+    */
+    ros::Duration rate = ros::Duration(0.1);
+    wrover_status_light::DoubleService srv;
+    srv.request.input = 1;
+
+    /*
+    This is blocking call that waits until a service called "double_service" is created and ready.
+    */
+    ros::service::waitForService("double_service");
+
+    /*
+    The funciton ros::ok() checks if the module is still running or not. 
+    */
+    while (ros::ok()) {
+
+        /*
+        Calls the service using the client instance created earlier. The call returns true if the 
+        operation is successful and false otherwise.
+        */
+        if (client.call(srv)) {
+            std::cout << srv.response.output << std::endl;
+            srv.request.input = srv.response.output;
+
+            /*
+            Waits for 100ms before resuming the program. 
+            */
+            rate.sleep();
+        } else {
+            /*
+            This code block is ran when the service call exits unsuccessfully.
+            */
+            std::cout << "Integer overflow encountered" << std::endl;
+        }
+    }
+}

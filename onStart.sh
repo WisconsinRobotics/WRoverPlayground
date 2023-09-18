2aec9f4d14911f0e614d666328c410cd677c60cf
Colin Li <clikingofinfo@gmail.com>
2023-09-18 02:16:36 +0000

    Update Dockerfile with venv

--- onStart.sh
+++ onStart.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 source /opt/ros/noetic/setup.sh
+
+if [ ! -d /workspaces/WRoverPlayground/venv ]; then
+  python3 -m venv venv
+fi
+
+source /workspaces/WRoverPlayground/venv/bin/activate
+pip install -r requirements.txt
+
 cd /workspaces/WRoverPlayground
 catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
